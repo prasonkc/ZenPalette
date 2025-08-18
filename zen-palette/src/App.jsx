af61db8b76ccbf4cab6269ee94b3669b9346d562
Prason <prasonkc07@gmail.com>
2025-08-18 22:47:35 +0545

    Designed Navbar

--- zen-palette/src/App.jsx
+++ zen-palette/src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+function NavBar() {
+  return (
+    <>
+      <div className="nav grid grid-cols-[80vw_10vw_10vw] font-bold font-mono text-xl">
+        <div className="flex justify-center justify-self-center">
+          <div className="palleteGen cursor-pointer hover:underline hover:bg-[#ffd6ba65]  p-5">
+            Colour Palette Generator
+          </div>
+          <div className="imagePick cursor-pointer hover:underline hover:bg-[#ffd6ba65]  p-5">
+            Image Picker
+          </div>
+        </div>
+        {/* Section for toggling light mode and dark mode */}
+        <div className="toggleBtn justify-self-center cursor-pointer p-5">
+          .../...
+        </div>
+        {/* About Me Section */}
+        <div className="aboutMe justify-self-center cursor-pointer  p-5">*</div>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
-      <div className="nav">
-        <div className="palleteGen">Colour Palette Generator</div>
-        <div className="imagePick">Image Picker</div>
-      </div>
+      <NavBar/>
 
-    <div className="palleteContainer">
-      {/* Generate 5 cards for picking colour */}
-    </div>
+      <div className="palleteContainer">
+        {/* Use react router here */}
+        {/* Generate 5 cards for picking colour */}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
