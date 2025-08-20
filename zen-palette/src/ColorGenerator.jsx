872b9154d080b76249df6c155073e988415650a6
Prason <prasonkc07@gmail.com>
2025-08-20 14:14:29 +0545

    Added event listener

--- zen-palette/src/ColorGenerator.jsx
+++ zen-palette/src/ColorGenerator.jsx
@@ -1,21 +1,52 @@
-function Card({color}){
-    return <>
-        <div className="card w-[20vw] h-[85vh] bg-[#fff] rounded-3xl shadow-2xl" style={{background: color}}></div>
+import { useEffect, useState } from "react";
+
+function Card() {
+  const [color, setColor] = useState("#fff");
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code == "Space") {
+        setColor(generateRandomHexColor());
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  return (
+    <>
+      <div
+        className="card w-[20vw] h-[85vh] bg-[#fff] rounded-3xl shadow-2xl cursor-pointer"
+        style={{ background: color }}
+        onClick={handleClick}
+      ></div>
     </>
+  );
+
+  function handleClick() {
+    setColor(generateRandomHexColor());
+  }
 }
 
 function generateRandomHexColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+  return (
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+  );
 }
 
-export default function ColorGenerator(){
-    return <>
-        <div className="paletteGenerator w-[95vw] h-full bg-[#FFDCDC] rounded-3xl m-auto pl-10 pt-7 pb-7 pr-10 flex gap-10">
-            <Card color = {generateRandomHexColor()}/>
-            <Card color = {generateRandomHexColor()}/>
-            <Card color = {generateRandomHexColor()}/>
-            <Card color = {generateRandomHexColor()}/>
-            <Card color = {generateRandomHexColor()}/>
-        </div>
+export default function ColorGenerator() {
+  return (
+    <>
+      <div className="paletteGenerator w-[95vw] h-full bg-[#FFDCDC] rounded-3xl m-auto pl-10 pt-7 pb-7 pr-10 flex gap-10">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
+  );
 }
