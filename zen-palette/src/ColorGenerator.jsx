538ac213e4b7c0527e5aa3e5774e2880af2e1c66
Prason <prasonkc07@gmail.com>
2025-08-20 13:48:57 +0545

    Added random colour generator

--- zen-palette/src/ColorGenerator.jsx
+++ zen-palette/src/ColorGenerator.jsx
@@ -1,13 +1,21 @@
 function Card({color}){
     return <>
-        <div className="card w-[20vw] h-[85vh] bg-[#fff] rounded-3xl" style={{background: color}}></div>
+        <div className="card w-[20vw] h-[85vh] bg-[#fff] rounded-3xl shadow-2xl" style={{background: color}}></div>
     </>
+}
+
+function generateRandomHexColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 }
 
 export default function ColorGenerator(){
     return <>
         <div className="paletteGenerator w-[95vw] h-full bg-[#FFDCDC] rounded-3xl m-auto pl-10 pt-7 pb-7 pr-10 flex gap-10">
-            <Card color = "#000"/>
+            <Card color = {generateRandomHexColor()}/>
+            <Card color = {generateRandomHexColor()}/>
+            <Card color = {generateRandomHexColor()}/>
+            <Card color = {generateRandomHexColor()}/>
+            <Card color = {generateRandomHexColor()}/>
         </div>
     </>
 }
