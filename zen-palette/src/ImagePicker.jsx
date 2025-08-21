14b3b3b5bc2bc32faf7be9405071d23e32184930
Prason <prasonkc07@gmail.com>
2025-08-21 22:49:47 +0545

    fixed dark mode colors

--- zen-palette/src/ImagePicker.jsx
+++ zen-palette/src/ImagePicker.jsx
@@ -1,29 +1,43 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function ImagePicker({ isDark }) {
-  const [file, setFile] = useState(null)
-  const [isOpen, setIsOpen] = useState(false)
+  const [file, setFile] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [pickedColor, setPickedColor] = useState(null);
+  const canvasref = useRef(null);
 
-  function handleDrop(e){
-    e.preventDefault()
-    const droppedFile = e.dataTransfer.files[0]
+  function handleDrop(e) {
+    e.preventDefault();
+    const droppedFile = e.dataTransfer.files[0];
 
-    if(!droppedFile){
-      return
+    if (!droppedFile) {
+      return;
     }
 
     if (!droppedFile.type.startsWith("image/")) {
       alert("Please select a valid image file.");
       return;
     }
-    setFile(droppedFile)
-    setIsOpen(true)
+    setFile(droppedFile);
+    setIsOpen(true);
   }
 
-  function handleDragOver(e){
-    e.preventDefault()
+  function handleDragOver(e) {
+    e.preventDefault();
   }
+
+  function handleImageLoad() {
+    const canvas = canvasref.current;
+    const ctx = canvas.getContext("2d");
+    const img = e.target;
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+  }
+
+  
 
   return (
     <>
@@ -31,10 +45,8 @@ export default function ImagePicker({ isDark }) {
         <div
           onDrop={handleDrop}
           onDragOver={handleDragOver}
-          className={clsx(
-            "dropZone flex justify-center items-center w-[50vh] h-[50vh] bg-gray-100 m-auto rounded-3xl cursor-pointer shadow-2xl shadow-purple-700",
-            isDark ? "" : "text-black"
-          )}
+          className=
+            "dropZone flex justify-center items-center w-[50vh] h-[50vh] m-auto rounded-3xl cursor-pointer shadow-2xl shadow-white-700"
         >
           Select or drop an image
         </div>
