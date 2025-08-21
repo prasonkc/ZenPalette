eb00dc97653c6cd748b3e1fdda0ff6186fc579a8
Prason <prasonkc07@gmail.com>
2025-08-21 13:24:20 +0545

    developed image uploades for image picker

--- zen-palette/src/ImagePicker.jsx
+++ zen-palette/src/ImagePicker.jsx
@@ -1,9 +1,54 @@
 import clsx from "clsx";
+import { useState } from "react";
 
-export default function ImagePicker({isDark}){
-    return <>
-        <div className={clsx("dropZone flex justify-center items-center w-[50vh] h-[50vh] bg-gray-100 m-auto rounded-3xl cursor-pointer shadow-2xl shadow-purple-700", isDark ? "" : "text-black")}>
-            Click or drop your image
+export default function ImagePicker({ isDark }) {
+  const [file, setFile] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleDrop(e){
+    e.preventDefault()
+    const droppedFile = e.dataTransfer.files[0]
+
+    if(!droppedFile){
+      return
+    }
+
+    if (!droppedFile.type.startsWith("image/")) {
+      alert("Please select a valid image file.");
+      return;
+    }
+    setFile(droppedFile)
+    setIsOpen(true)
+  }
+
+  function handleDragOver(e){
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      {!file && (
+        <div
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          className={clsx(
+            "dropZone flex justify-center items-center w-[50vh] h-[50vh] bg-gray-100 m-auto rounded-3xl cursor-pointer shadow-2xl shadow-purple-700",
+            isDark ? "" : "text-black"
+          )}
+        >
+          Select or drop an image
         </div>
+      )}
+      {file && (
+        <div className="flex justify-center items-center w-full h-[90vh] m-auto">
+          <img
+            src={URL.createObjectURL(file)}
+            alt=""
+            className="object-contain w-full h-full"
+            style={{ aspectRatio: "1 / 1" }}
+          />
+        </div>
+      )}
     </>
+  );
 }
